880362211d45c79506a45690b65131312199d6fa
Kolade <olubanjok@gmail.com>
2024-11-15 22:02:21 +0100

    chore: add assets and icons

---
+++ src/components/icons/ShieldIcon.tsx
@@ -0,0 +1,18 @@
+export default function ShieldIcon() {
+  return (
+    <svg
+      width="17"
+      height="19"
+      viewBox="0 0 17 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.26758 19L4.09625 15.9041C1.69356 14.1208 0.267578 11.2417 0.267578 8.17363V3.5625L8.26758 0L16.2676 3.5625V8.17363C16.2676 11.2417 14.8416 14.1208 12.4389 15.9041L8.26758 19ZM13.0757 6.77719L11.4595 5.09781L7.12472 9.60188L5.0757 7.47281L3.45945 9.15219L7.12472 12.9606L13.0757 6.77719Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
